0e03f25b94c3ea55c2b67bcda4df47ba3d5d0ce4
Jan Bujak <j@exia.io>
2017-03-12 13:59:57 +0100

    Let's try this again
    Have I ever said how much I despise C++ build systems?

--- build.sh
+++ build.sh
@@ -21,7 +21,6 @@ mv "chromium-$VERSION" chromium
 
 pushd chromium
 
-sed -i '/config("compiler")/ a cflags_cc = [ "-fno-delete-null-pointer-checks" ]' build/config/linux/BUILD.gn
 find . -name '*.py' -exec sed -i -r 's|/usr/bin/python$|&2|g' {} +
 python2 third_party/libaddressinput/chromium/tools/update-strings.py
 
@@ -40,7 +39,10 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git /tmp/
 export PATH="/tmp/depot_tools:$PATH"
 
 bash build/install-build-deps.sh --no-arm --no-nacl --no-prompt --no-syms
-gclient runhooks
+
+python2 build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
+python2 tools/clang/scripts/update.py
+python2 tools/gn/bootstrap/bootstrap.py --gn-gen-args="fatal_linker_warnings=false treat_warnings_as_errors=false clang_use_chrome_plugins=false"
 
 mkdir -p out/Release
 
@@ -48,6 +50,7 @@ echo 'import("//build/args/headless.gn")' > out/Release/args.gn
 
 echo 'is_debug=false' >> out/Release/args.gn
 echo 'clang_use_chrome_plugins=false' >> out/Release/args.gn
+echo 'blink_gc_plugin=false' >> out/Release/args.gn
 echo 'symbol_level=0' >> out/Release/args.gn
 
 echo 'fatal_linker_warnings=false' >> out/Release/args.gn
@@ -55,6 +58,13 @@ echo 'treat_warnings_as_errors=false' >> out/Release/args.gn
 echo 'remove_webcore_debug_symbols=true' >> out/Release/args.gn
 
 echo 'enable_nacl=false' >> out/Release/args.gn
+echo 'enable_media_remoting=false' >> out/Release/args.gn
+echo 'enable_one_click_signin=false' >> out/Release/args.gn
+echo 'enable_pdf=false' >> out/Release/args.gn
+echo 'enable_basic_print_dialog=false' >> out/Release/args.gn
+echo 'enable_basic_printing=false' >> out/Release/args.gn
+echo 'enable_print_preview=false' >> out/Release/args.gn
+echo 'safe_browsing_mode=0' >> out/Release/args.gn
 
 echo 'use_cups=false' >> out/Release/args.gn
 echo 'use_dbus=false' >> out/Release/args.gn
@@ -65,10 +75,9 @@ echo 'use_gtk3=false' >> out/Release/args.gn
 echo 'use_kerberos=false' >> out/Release/args.gn
 echo 'use_libpci=false' >> out/Release/args.gn
 echo 'use_pulseaudio=false' >> out/Release/args.gn
-echo 'use_seccomp_bpf=false' >> out/Release/args.gn
 echo 'use_udev=false' >> out/Release/args.gn
 
-gn gen out/Release --script-executable=/usr/bin/python2
+out/Release/gn gen out/Release --script-executable=/usr/bin/python2
 ninja -C out/Release headless_shell
 
 popd # chromium
