df3416f2141c400f7020aca58e3f8df6e256a976
Jan Bujak <j@exia.io>
2017-03-13 21:22:54 +0100

    Fix built executable caching

--- cache.sh
+++ cache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+pushd chromium-build/chromium
 cp out/Release/{character_data_generator,proto_zero_plugin,protoc,genmacro,genmodule,genstring,genversion,genperf,re2c,yasm,mojo_runner} $TRAVIS_BUILD_DIR/cache/ || true
